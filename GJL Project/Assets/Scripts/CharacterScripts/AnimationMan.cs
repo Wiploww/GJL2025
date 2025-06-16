3d279d9ab527b95be36938b0566c774516379fb0
Charlotte Peterson <charlotte.peterson@mymail.champlain.edu>
2025-06-16 17:24:35 -0400

    wep

--- GJL Project/Assets/Scripts/CharacterScripts/AnimationMan.cs
+++ GJL Project/Assets/Scripts/CharacterScripts/AnimationMan.cs
@@ -3,14 +3,41 @@ using UnityEngine;
 public class AnimationMan : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
+    private Animator animator;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+        if (rb.linearVelocity.x > 0 )
+        {
+
+            //animator.SetBool("LeftWalk", true);
+
+        }
+        if (rb.linearVelocity.x < 0)
+        {
+
+            //animator.SetBool("RightWalk", true);
+        }
+
+        if(rb.linearVelocity.y > 0)
+        {
+            Debug.Log("Front");
+            animator.SetBool("FrontWalk",true);
+        }
+
+        if (rb.linearVelocity.y < 0)
+        {
+            Debug.Log("Back");
+
+            animator.SetBool("BackwardWalk", true);
+        }
     }
 }
