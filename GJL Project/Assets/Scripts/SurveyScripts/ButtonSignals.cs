47b7fc32a0af2ed3137e741fc6c91f45892a701e
blade-x7 <portia.flaherty@icloud.com>
2025-06-17 16:43:27 -0400

    contingency commit, main game survey code and UI in progres

--- GJL Project/Assets/Scripts/SurveyScripts/ButtonSignals.cs
+++ GJL Project/Assets/Scripts/SurveyScripts/ButtonSignals.cs
@@ -7,15 +7,10 @@ public class ButtonSignals : MonoBehaviour
     [SerializeField] GameObject gameManager;
     [SerializeField] int eventCode;
 
-    void Start()
-    {
-        
-    }
-
     public void sendSignal()
     {
         //send ping w/ event code to game manager
-        gameManager.GetComponent<TestGameManager>().RecieveSignal(eventCode);
+        gameManager.GetComponent<GameManager>().RecieveSignal(eventCode);
 
         //make sure partner button gets deselected, if applicable
         if (partner.GetComponent<SurveyButton>().isSelected())
---
+++ GJL Project/Assets/Scripts/SurveyScripts/GameManager.cs
@@ -0,0 +1,336 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    int roundCounter;
+
+    [SerializeField] GameObject player;
+    Vector3 playerStartPos;
+
+    //survey screens
+    [SerializeField] GameObject survey1;
+    [SerializeField] GameObject survey2;
+    [SerializeField] GameObject survey3;
+    [SerializeField] GameObject survey4;
+
+    //survey 1 bools
+    bool addMoreDesks; //true adds more desks, false removes all desks; codes 11 and 12
+    bool floorColorChange; //true for chartreuse, false for aubergine; codes 13 and 14
+
+    //survey 2 bools
+    bool birdsOrBots; //true for animals, false for machines; codes 21 and 22
+    bool addGoblins; //false adds a question to survey 3; codes 23 and 24
+
+    //survey 3 bools
+    //does crossbow v slingshot need a bool?
+    bool whichImage; //true for skull, false for backflip; codes 31 and 32
+    bool shouldThisQuestionShowUp; //codes 33 and 34
+    bool sureAboutGoblins; //only shows up if addGoblins is false; codes 35 and 23
+
+    //survey 4 bools
+    bool canBribe; //code 41
+    bool canHack; //code 42
+    bool cactusHeight; //true makes cactus taller, false makes it wider; codes 43 and 44
+    // reuse shouldThisQuestionShowUp variable?
+    bool didPlayerHaveFun; //codes 51 and 52
+
+    //survey 5 (stretch goal)
+
+    //layers
+    [SerializeField] GameObject desks;
+    [SerializeField] GameObject moreDesks;
+    [SerializeField] GameObject animals;
+    [SerializeField] GameObject machines;
+    [SerializeField] GameObject goblins;
+    [SerializeField] GameObject skullImage;
+    [SerializeField] GameObject backflipImage;
+    [SerializeField] GameObject ogCactus;
+    [SerializeField] GameObject tallCactus;
+    [SerializeField] GameObject wideCactus;
+    
+    void Start()
+    {
+        roundCounter = 1;
+        playerStartPos = player.transform.position;
+        canBribe = false;
+        canHack = false;
+
+        //make sure all the things are set correctly
+        survey1.SetActive(false);
+        survey2.SetActive(false);
+        survey3.SetActive(false);
+        survey4.SetActive(false);
+
+        desks.gameObject.SetActive(true);
+        moreDesks.gameObject.SetActive(false);
+        animals.gameObject.SetActive(false);
+        machines.gameObject.SetActive(false);
+        goblins.gameObject.SetActive(false);
+        skullImage.gameObject.SetActive(false);
+        backflipImage.gameObject.SetActive(false);
+        ogCactus.gameObject.SetActive(false);
+        tallCactus.gameObject.SetActive(false);
+        wideCactus.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BeginSurvey()
+    {
+        switch (roundCounter)
+        {
+            case 1:
+                {
+                    survey1.SetActive(true);
+                    break;
+                }
+            case 2:
+                {
+                    survey2.SetActive(true);
+                    break;
+                }
+            case 3:
+                {
+                    survey3.SetActive(true);
+                    break;
+                }
+            case 4:
+                {
+                    survey4.SetActive(true);
+                    break;
+                }
+            case 5:
+                {
+                    //if it's the final round, end the game
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+        //reset game here or in NextRound
+    }
+
+    public void NextRound()
+    {
+        CheckSurveyInput();
+        CloseSurvey();
+        roundCounter++;
+    }
+
+    void ResetGame()
+    {
+        player.transform.position = playerStartPos;
+        //reset anything else that needs to be reset
+    }
+
+    public void RecieveSignal(int code)
+    {
+        switch (code)
+        {
+            case 11:
+                {
+                    addMoreDesks = true;
+                    break;
+                }
+            case 12:
+                {
+                    addMoreDesks = false;
+                    break;
+                }
+            case 13:
+                {
+                    floorColorChange = true; //chartreuse
+                    break;
+                }
+            case 14:
+                {
+                    floorColorChange = false; //aubergine
+                    break;
+                }
+            case 21:
+                {
+                    birdsOrBots = true; //animals
+                    break;
+                }
+            case 22:
+                {
+                    birdsOrBots = false; //machines
+                    break;
+                }
+            case 23:
+                {
+                    addGoblins = true;
+                    sureAboutGoblins = false;
+                    break;
+                }
+            case 24:
+                {
+                    addGoblins = false;
+                    break;
+                }
+            case 31:
+                {
+                    whichImage = true; //flaming skull
+                    break;
+                }
+            case 32:
+                {
+                    whichImage = false; //backflip
+                    break;
+                }
+            case 33:
+                {
+                    shouldThisQuestionShowUp = true;
+                    break;
+                }
+            case 34:
+                {
+                    shouldThisQuestionShowUp = false;
+                    break;
+                }
+            case 35:
+                {
+                    sureAboutGoblins = true;
+                    break;
+                }
+            case 41:
+                {
+                    canBribe = true;
+                    canHack = false;
+                    break;
+                }
+            case 42:
+                {
+                    canBribe = false;
+                    canHack = true;
+                    break;
+                }
+            case 43:
+                {
+                    cactusHeight = true; //make it tall
+                    break;
+                }
+            case 44:
+                {
+                    cactusHeight = false; //make it wide
+                    break;
+                }
+            case 51:
+                {
+                    //play confetti effect
+                    didPlayerHaveFun = true;
+                    break;
+                }
+            case 52:
+                {
+                    //play sad horn effect
+                    didPlayerHaveFun = false; //:(
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    //lock in responses from surveys
+    void CheckSurveyInput()
+    {
+        switch (roundCounter)
+        {
+            case 1: //survey 1
+                {
+                    if (addMoreDesks)
+                    {
+                        moreDesks.gameObject.SetActive(true); //add em in
+                    }
+                    else
+                    {
+                        desks.gameObject.SetActive(false); //remove em all
+                    }
+                    if (floorColorChange)
+                    {
+                        //set floor color to chartreuse
+                    }
+                    else
+                    {
+                        //set floor color to aubergine
+                    }
+                    break;
+                }
+            case 2: //survey 2
+                {
+                    if (birdsOrBots)
+                    {
+                        animals.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        machines.gameObject.SetActive(true);
+                    }
+                    if (addGoblins)
+                    {
+                        goblins.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        //add "are you sure about goblins" to survey 3
+                    }
+                    break;
+                }
+            case 3: //survey 3
+                {
+                    if (whichImage)
+                    {
+                        skullImage.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        backflipImage.gameObject.SetActive(true);
+                    }
+                    if (shouldThisQuestionShowUp)
+                    {
+                        //add question to survey 4
+                    }
+                    if (!sureAboutGoblins)
+                    {
+                        goblins.gameObject.SetActive(true);
+                    }
+                    break;
+                }
+            case 4: //survey 4
+                {
+                    if (cactusHeight)
+                    {
+                        tallCactus.gameObject.SetActive(true);
+                        ogCactus.gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        wideCactus.gameObject.SetActive(true);
+                        ogCactus.gameObject.SetActive(false);
+                    }
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    void CloseSurvey()
+    {
+        survey1.SetActive(false);
+        survey2.SetActive(false);
+        survey3.SetActive(false);
+        survey4.SetActive(false);
+    }
+}
