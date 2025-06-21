4f08776f8d42dca20bbedc33a9f9252d0c9b47e3
Charlotte Peterson <charlotte.peterson@mymail.champlain.edu>
2025-06-20 20:08:22 -0400

    Working on Animations

--- GJL Project/Assets/Scripts/CharacterScripts/AnimationMan.cs
+++ GJL Project/Assets/Scripts/CharacterScripts/AnimationMan.cs
@@ -1,13 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationMan : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
     private Animator animator;
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb =GetComponent<Rigidbody2D>();
 
     }
 
@@ -27,17 +29,52 @@ public class AnimationMan : MonoBehaviour
             //animator.SetBool("RightWalk", true);
         }
 
-        if(rb.linearVelocity.y > 0)
+        if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Front");
-            animator.SetBool("FrontWalk",true);
-        }
-
-        if (rb.linearVelocity.y < 0)
-        {
-            Debug.Log("Back");
-
             animator.SetBool("BackwardWalk", true);
         }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("BackwardWalk", false);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("ForwardWalk", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("ForwardWalk", false);
+
+        }
+
+        /*  if (rb.linearVelocity.y > 0)
+          {
+
+              Debug.Log("Back");
+
+              animator.SetBool("BackwardWalk", true);
+          }
+
+          if  (rb.linearVelocity.y<0)
+          {
+              Debug.Log("Front");
+              animator.SetBool("ForwardWalk", true);
+          }
+
+
+          if(rb.linearVelocity.y == 0)
+          {
+              Debug.Log("None");
+              animator.SetBool("ForwardWalk", false);
+              animator.SetBool("BackwardWalk", false);
+
+          }
+      */
     }
+
 }
+
