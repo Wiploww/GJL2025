804e85977dc10a4f62dd3533984cc3a9b75e70e1
Charlotte Peterson <charlotte.peterson@mymail.champlain.edu>
2025-06-21 14:21:44 -0400

    Bridge Machine and Aligator done

--- GJL Project/Assets/Scripts/WorldScripts/Aligator.cs
+++ GJL Project/Assets/Scripts/WorldScripts/Aligator.cs
@@ -4,17 +4,22 @@ public class Aligator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    private GameObject Blocker;
-    private GameObject Gator;
+    public GameObject Blocker;
+    public GameObject Gator;
     private GameManager GameManager;
     void Start()
     {
-        Blocker= this.transform.GetChild(0).gameObject;
-        Gator=this.transform.GetChild(1).gameObject;
+       
         Gator.SetActive(false);
         GameManager= GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
+    private void OnEnable()
+    {
+       
+        Blocker.SetActive(false);
+        Gator.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ GJL Project/Assets/Scripts/WorldScripts/BridgeMachine.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class BridgeMachine : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string text;
+    public GameObject Blocker;
+    public GameObject bridge;   
+    private GameObject Player;
+    private GameObject PressE;
+    public bool canTrigger = false;
+    void Start()
+    {
+        PressE = this.transform.GetChild(0).gameObject;
+        PressE.SetActive(false);
+        bridge = this.transform.GetChild(1).gameObject;
+        bridge.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            Blocker.SetActive(false);
+            bridge.SetActive(true);
+            canTrigger = false;
+            PressE?.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PressE.SetActive(true);
+            canTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PressE.SetActive(false);
+            canTrigger = false;
+        }
+    }
+}
